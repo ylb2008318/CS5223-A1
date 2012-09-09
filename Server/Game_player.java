0e262f06525b1b51f81aa5ccccef1661e5a06192
ylb2008318 <ylibi@ymail.com>
2012-09-09 18:32:51 +0800

    server v3

--- Server/Game_player.java
+++ Server/Game_player.java
@@ -5,4 +5,20 @@ public class Game_player extends Map_obj {
     }
     
     private int treasure_got;
+    
+    private int playerID;
+
+    public Game_player(int playerID) {
+        this.playerID = playerID;
+        treasure_got = 0;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public int getTreasure_got() {
+        return treasure_got;
+    }
+    
 }
